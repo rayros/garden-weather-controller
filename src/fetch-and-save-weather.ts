48dc3242dcbfa24bdd45ffa7ee61f3d7e935b5e9
Paweł Łaski <pawel.laski@bacca.pl>
2022-09-06 20:33:59 +0200

    Move weather place to env

--- src/fetch-and-save-weather.ts
+++ src/fetch-and-save-weather.ts
@@ -12,7 +12,7 @@ export const fetchAndSaveWeather = async () => {
 }
 
 async function fetchWeather() {
-  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=51.9052433&lon=20.9363443&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=metric`);
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${process.env.WEATHER_PLACE_LAT}&lon=${process.env.WEATHER_PLACE_LON}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=metric`);
   return {
     rain: response.data.rain ? response.data.rain["1h"] : 0,
     temp: response.data.main.temp,
